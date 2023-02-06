f5b0933c292db8f3e388ece83b2fa424c370c10b
Leonardo Lopez <leonardojoselc@ufps.edu.co>
2023-02-06 13:59:44 -0500

    V1.10.6

--- src/app/modules/auth/login/form-login.component.ts
+++ src/app/modules/auth/login/form-login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CONST_LOGIN_PAGE } from '@data/constants';
+import { PutPacienteUseCases } from '@data/usecases/paciente/put-paciente-use-cases';
 
 @Component({
   selector: 'app-form-login',
@@ -9,12 +10,14 @@ export class FormLoginComponent {
   public data=CONST_LOGIN_PAGE 
   public loginForm;
   
+  constructor(useCasePaciente: PutPacienteUseCases){
+    this.loginForm=this.data.FORM;
+    }
      get isValidForm(){
       return (this.loginForm.email.isValid()&& this.loginForm.password.isValid());
     }
+
   
-    constructor(){
-      this.loginForm=this.data.FORM;
-      }
+    
     }
 
