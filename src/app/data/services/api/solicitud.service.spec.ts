09454843fbd510e50c53716a0a55696983a83844
Leonardo Lopez <leonardojoselc@ufps.edu.co>
2023-02-16 19:52:38 -0500

    V1.12.9

--- src/app/data/services/api/solicitud.service.spec.ts
+++ src/app/data/services/api/solicitud.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SolicitudService } from './solicitud.service';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '@shared/shared.module';
 
 describe('SolicitudService', () => {
   let service: SolicitudService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SolicitudService);
+
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule,SharedModule]
+    })
+    .compileComponents();
+ 
   });
 
   it('should be created', () => {
