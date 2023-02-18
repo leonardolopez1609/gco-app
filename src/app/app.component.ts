0d70d323ddd8908901f372713d59f758cafa707e
Leonardo Lopez <leonardojoselc@ufps.edu.co>
2023-02-17 22:05:21 -0500

    V1.12.9.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
+//Commit
 export class AppComponent {
   title = 'gco-app';
 }
